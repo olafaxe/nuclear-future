f8bafba13f772e9d1da7970510741e15a5ff04e4
Ola Faxe <olafaxe@gmail.com>
2019-09-27 13:46:07 +0200

    spa work

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 "use strict";
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 app.set("view engine", "ejs");
 app.use(express.static("views/public"));
 app.get("/", (req, res) => {
     res.render("pages/index");
+});
+app.get("/about", (req, res) => {
+    console.log(res);
+    res.render("pages/about.ejs");
 });
 app.listen(port, () => {
     console.log(`Lyssnar på port: ${port}`);
--- views/public/main.js
+++ views/public/main.js
@@ -1,2 +1,19 @@
 "use strict";
-console.log("hej");
+let mainBody = document.querySelector(".grid-container");
+mainBody.addEventListener("click", e => {
+    if (e.target.classList.contains("btn-nav")) {
+        console.log("click");
+        let xhr = new XMLHttpRequest();
+        xhr.open("GET", `http://localhost:3000/about`);
+        xhr.setRequestHeader("Content-type", "application/json");
+        xhr.send();
+        xhr.onload = function () {
+            if (xhr.status != 200) {
+                alert(`Error ${xhr.status}: ${xhr.statusText}`);
+            }
+            else {
+                console.log("success");
+            }
+        };
+    }
+});
