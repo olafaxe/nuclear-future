1900af1b2034605ae377f2265e7cae31e195015b
Ola Faxe <olafaxe@gmail.com>
2019-09-29 21:19:36 +0200

    force secure addition

--- app.ts
+++ app.ts
@@ -1,11 +1,11 @@
+const forceSecure = require("force-secure-express");
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.set("view engine", "ejs");
 app.use(express.static("views/public"));
-
-// app.use(express.static("public"));
+app.use(forceSecure(["faxe-atomics.herokuapp.com"]));
 
 app.get("/", (req, res) => {
   res.render("pages/index");
